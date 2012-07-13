2da621169025de2ab2ed5dbb7d19be993ecab7fc
Jeremias Kangas <jeremias.kangas@gmail.com>
2012-07-13 19:12:38 +0200

    asd

--- electrumpos/urls.py
+++ electrumpos/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'electrumpos.views.home', name='home'),
+    url(r'^$', 'electrumpos.payments.home', name='home'),
     # url(r'^electrumpos/', include('electrumpos.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
